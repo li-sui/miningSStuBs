ac9e3cfc5d17e169569b80890f7da0c611008b25
Li Sui <leesui0207@gmail.com>
2021-06-03 13:06:21 +1200

    starting writing result section

---
+++ src/main/java/nz/ac/massey/se/miningSStuBs/CountNoOfBugs.java
@@ -0,0 +1,26 @@
+package nz.ac.massey.se.miningSStuBs;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.nio.charset.Charset;
+import java.util.List;
+
+/**
+ * count number of single line bugs in results/bugs.json
+ * @author Li Sui
+ */
+public class CountNoOfBugs {
+    //TODO: make it configurable.
+    static String  jsonFile = "results/bugs.json";
+    public static void main(String[] args) throws Exception {
+
+        Gson gson = new Gson();
+        String input = FileUtils.readFileToString(new File(jsonFile), Charset.defaultCharset());
+        List<BugData> reportList= gson.fromJson(input,new TypeToken<List<BugData>>(){}.getType());
+        System.out.println(reportList.size());
+    }
+
+}
--- src/main/java/nz/ac/massey/se/miningSStuBs/Util.java
+++ src/main/java/nz/ac/massey/se/miningSStuBs/Util.java
@@ -7,7 +7,6 @@ import org.apache.commons.io.FileUtils;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
-import java.util.HashMap;
 import java.util.Map;
 
 /**
